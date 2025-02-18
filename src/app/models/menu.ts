1eca54e89a728313d0411d8d3e902e7fe67a438b
Romy514 <153219401+Romy514@users.noreply.github.com>
2025-02-18 14:05:29 +0100

    correction syntaxe models

--- src/app/models/menu.ts
+++ src/app/models/menu.ts
@@ -1,7 +1,10 @@
 export class Menu {
-    private id:number = 0;
-    private nom:String ="";
-    private description:String ="";
-    private date_creation:String ="";
-    private statut:String ="";
+    constructor(
+        private id:number = 0,
+        private nom:String ="",
+        private description:String ="",
+        private date_creation:String ="",
+        private statut:String =""
+    ){}
+
 }
--- src/app/models/plat.ts
+++ src/app/models/plat.ts
@@ -1,6 +1,8 @@
 export class Plat {
-    private id:number = 0;
-    private menu_id:number =0;
-    private nom:String ="";
-    private calories:number =0;
+    constructor(
+    private id:number = 0,
+    private menu_id:number =0,
+    private nom:String ="",
+    private calories:number =0
+    ){}
 }
