6dcb7390d79dd3bfba1b8f548802c97f0a323cd8
Romy514 <153219401+Romy514@users.noreply.github.com>
2025-02-18 14:10:34 +0100

    ajout components

---
+++ src/app/menus/menu-detail/menu-detail.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-menu-detail',
+  templateUrl: './menu-detail.component.html',
+  styleUrl: './menu-detail.component.css'
+})
+export class MenuDetailComponent {
+
+}
